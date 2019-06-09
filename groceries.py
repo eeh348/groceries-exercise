a66700ea40c06dc97f44e75fdb1789755c1cccff
Eileen Hengel <eileenhengel@Bobs-MacBook-Pro.local>
2019-06-09 15:53:18 -0400

    add formatting

--- groceries.py
+++ groceries.py
@@ -25,7 +25,41 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
-print(products)
+#print(products)
 # pprint(products)
 
 # TODO: write some Python code here to produce the desired output
+
+#--------------
+#THERE ARE 20 PRODUCTS:
+#--------------
+# + All-Seasons Salt ($4.99)
+# + Chocolate Fudge Layer Cake ($18.50)
+# + Chocolate Sandwich Cookies ($3.50)
+# + Cut Russet Potatoes Steam N' Mash ($4.25)
+# + Dry Nose Oil ($21.99)
+# + Fresh Scent Dishwasher Cleaner ($4.99)
+# + Gluten Free Quinoa Three Cheese & Mushroom Blend ($3.99)
+# + Green Chile Anytime Sauce ($7.99)
+# + Light Strawberry Blueberry Yogurt ($6.50)
+# + Mint Chocolate Flavored Syrup ($4.50)
+# + Overnight Diapers Size 6 ($25.50)
+# + Peach Mango Juice ($1.99)
+# + Pizza For One Suprema Frozen Pizza ($12.50)
+# + Pomegranate Cranberry & Aloe Vera Enrich Drink ($4.25)
+# + Pure Coconut Water With Orange ($3.50)
+# + Rendered Duck Fat ($9.99)
+# + Robust Golden Unsweetened Oolong Tea ($2.49)
+# + Saline Nasal Mist ($16.00)
+# + Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce ($6.99)
+# + Sparkling Orange Juice & Prickly Pear Beverage ($2.99)
+
+prod_count = len(products)
+
+print("--------------")
+print("THERE ARE " + str(prod_count) + " PRODUCTS:")
+print("--------------")
+
+for p in products:
+    price_usd = " (${0:.2f})".format(p["price"])
+    print ("+ " + p["name"] + price_usd )
